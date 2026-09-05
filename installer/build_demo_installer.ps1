888109a35cfc3eb9b0fa6281776e687cab1428c8
Paul Patel <paulcamerasystems@gmail.com>
2026-09-05 09:17:07 -0500

    Prepare cash payout release 1.0.163

--- installer/build_demo_installer.ps1
+++ installer/build_demo_installer.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Configuration = "Release",
     [string]$Runtime = "win-x64",
-    [string]$Version = "1.0.161"
+    [string]$Version = "1.0.163"
 )
 
 $ErrorActionPreference = "Stop"
